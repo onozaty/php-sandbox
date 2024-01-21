2fd82aafe29c76f7ccaf3f2b6f01155d6adb992d
onozaty <onozaty@gmail.com>
2024-01-21 23:21:06 +0900

    [PHP devcontainer] MySQLを設定

--- devcontainer/php-mysql/index.php
+++ devcontainer/php-mysql/index.php
@@ -4,6 +4,21 @@ function sayHello($name) {
 	echo "Hello $name!";
 }
 
+function mysqlVersion() {
+	$dsn = 'mysql:host=db;dbname=db';
+	$db_user = 'user';
+	$db_pass = 'password';
+
+	$pdo = new PDO($dsn, $db_user, $db_pass);
+
+	$stmt = $pdo->prepare('SELECT version()');
+	$res = $stmt->execute();
+
+	$data = $stmt->fetch();
+	return $data[0];
+}
+
+
 ?>
 
 <html>
@@ -13,6 +28,7 @@ function sayHello($name) {
 	<body>
 		<?php 
 		sayHello('devcontainer');
+		echo '<p>mysql version: ' . mysqlVersion() . '</p>';
 		phpinfo(); 
 		?>
 	</body>
